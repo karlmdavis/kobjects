26fd45a5a780dc508d7eef6adc0b0d6e5bb1a6da
haustein <haustein>
2001-08-23 10:20:26 +0000

    Base64 updates

--- src/org/kobjects/base64/Base64.java
+++ src/org/kobjects/base64/Base64.java
@@ -1,22 +1,26 @@
-// kObjects
+// kObjects 
 //
 // Copyright (C) 2001 Stefan Haustein, Oberhausen (Rhld.), Germany
 //
 // Contributors: 
 //
-// STATUS: 
+// License: LGPL
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
-// License, or (at your option) any later version. This program is
-// distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-// License for more details. You should have received a copy of the
-// GNU General Public License along with this program; if not, write
-// to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-// Boston, MA 02111-1307, USA.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+
 
 package org.kobjects.base64;
 
@@ -25,50 +29,70 @@ import java.io.*;
 
 public class Base64 {
 
-    static final String charTab = 
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"; 
+    static final char[] charTab = 
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray (); 
+
+    
+    public static String encode (byte [] data) {
+	return encode (data, 0, data.length, null).toString ();
+    }
 
 
-    public static String encode (byte [] data) throws IOException {
+    /** Encodes the part of the given byte array denoted by start and
+	len to the Base64 format.  The encoded data is appended to the
+	given StringBuffer. If no StringBuffer is given, a new one is
+	created automatically. The StringBuffer is the return value of
+	this method. */
+ 
 
-	StringBuffer buf = new StringBuffer (data.length * 3 / 2);
+    public static StringBuffer encode (byte [] data, int start, int len, StringBuffer buf) {
 
-	int i = 0;
-	int end = data.length - 3;
+	if (buf == null) 
+	    buf = new StringBuffer (data.length * 3 / 2);
+
+	int end = len - 3;
+	int i = start;
+	int n = 0;
 
 	while (i <= end) {
 	    int d = ((((int) data [i]) & 0x0ff) << 16) 
 		| ((((int) data [i+1]) & 0x0ff) << 8)
 		| (((int) data [i+2]) & 0x0ff);
 
-	    buf.append (charTab.charAt ((d >> 18) & 63));
-	    buf.append (charTab.charAt ((d >> 12) & 63));
-	    buf.append (charTab.charAt ((d >> 6) & 63));
-	    buf.append (charTab.charAt (d & 63));
+	    buf.append (charTab [(d >> 18) & 63]);
+	    buf.append (charTab [(d >> 12) & 63]);
+	    buf.append (charTab [(d >> 6) & 63]);
+	    buf.append (charTab [d & 63]);
 
 	    i += 3;
 
-	    if (i % 45 == 0) buf.append ("\r\n");
+	    if (n++ >= 14) {
+		n = 0;
+		buf.append ("\r\n");
+	    }
 	}
 
-	if (i == data.length - 2) {
+
+	if (i == start + len - 2) {
 	    int d = ((((int) data [i]) & 0x0ff) << 16) 
 		| ((((int) data [i+1]) & 255) << 8);
 
-	    buf.append (charTab.charAt ((d >> 18) & 63));
-	    buf.append (charTab.charAt ((d >> 12) & 63));
-	    buf.append (charTab.charAt ((d >> 6) & 63));
+	    buf.append (charTab [(d >> 18) & 63]);
+	    buf.append (charTab [(d >> 12) & 63]);
+	    buf.append (charTab [(d >> 6) & 63]);
 	    buf.append ("=");
 	}
-	else if (i == data.length - 1) {
+	else if (i == start + len - 1) {
 	    int d = (((int) data [i]) & 0x0ff) << 16;
 
-	    buf.append (charTab.charAt ((d >> 18) & 63));
-	    buf.append (charTab.charAt ((d >> 12) & 63));
+	    buf.append (charTab [(d >> 18) & 63]);
+	    buf.append (charTab [(d >> 12) & 63]);
 	    buf.append ("==");
 	}
-	return buf.toString ();
+
+	return buf;
     }
+
 
     static int decode (char c) {
 	if (c >= 'A' && c <= 'Z') 
@@ -86,6 +110,9 @@ public class Base64 {
 	}
     }
 		
+
+    /** Decodes the given Base64 encoded String to a new byte array. 
+	The byte array holding the decoded data is returned. */
 
 
     public static byte [] decode (String s) {
