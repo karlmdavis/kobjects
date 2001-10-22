75eb83ddd06dadd1d45c085d87e5a9e75afe72cf
haustein <haustein>
2001-10-22 18:52:06 +0000

    test modifications...

--- samples/rmsindex/RMSIndexTest.java
+++ samples/rmsindex/RMSIndexTest.java
@@ -6,46 +6,39 @@ import java.util.*;
 import javax.microedition.rms.*;
 import org.kobjects.rms.RmsIndex;
 
-class RMSIndexTest
-{
+class RMSIndexTest {
 
-	public RMSIndexTest()
-	{
+    static final int COUNT = 9;
+
+
+    public static void main ( String [] argv ) throws RecordStoreException {
+
+	String line = "";
+	RmsIndex ri = null;
 	
+	ri = new RmsIndex( "idxtest_9" );
+	
+	System.out.println ("Sequential write");
+	
+	for (int element = 0; element < COUNT; element++ ) {    	
+	    String sKey = "Key " + element;
+	    String data = "Element " + element;
+	    
+	    ri.put(sKey, data);
 	}
 	
-	public static void main ( String [] argv ) 
-	{
-		String line = "";
-		RmsIndex ri = null;
+	System.out.println ("Sequential read");
 
-		try
-	   	{
-      			ri = new RmsIndex( "idxtest_9" );
-      		}catch( RecordStoreException e ){ System.out.println( "ERROR #1" ); }
-
-      		for( int element = 0; element < 9; element++ ) 
-        	{    	
-        		String sKey = "Key " + element;
-	    		String data = "Element " + element;
+	for (int element = 0; element < COUNT; element++ ) {    	
+	    String sKey = "Key " + element;
 	    		
-        		try
-        		{
-	 		  ri.put(sKey, data);
-       			}catch( RecordStoreException e ){ System.out.println( "ERROR #2" ); }
-       		}
-
-      		for( int element = 0; element < 9; element++ ) 
-        	{    	
-	    		String sKey = "Key " + element;
-	    		
-        		try
-        		{
-	 		  String data=ri.get(sKey);
-	 		  System.out.println(data);
-       			}catch( RecordStoreException e ){ System.out.println( "ERROR #2" ); }
-       		}
-    	
-      		
+	    String data=ri.get(sKey);
+	    System.out.println(data);
 	}
+
+	
+	ri.close ();
+    }
 }
+
+
