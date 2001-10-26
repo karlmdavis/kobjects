38933351113932efe8c61344af729ce7faf6d0de
mkroll <mkroll>
2001-10-26 18:54:22 +0000

    Added getVersion(), getLastModified(). Changed method for storing RecordStores from Vector serialization to use of DataOutputStream.

--- me4se/src/org/me4se/impl/RecordStoreImpl_file.java
+++ me4se/src/org/me4se/impl/RecordStoreImpl_file.java
@@ -1,40 +1,3 @@
-// kAWT - Kilobyte Abstract Window Toolkit
-//
-// Copyright (C) 1999-2000 by Michael Kroll & Stefan Haustein GbR, Essen
-//
-// Contact: kawt@kawt.de
-// General Information about kAWT is available at: http://www.kawt.de
-//
-// Using kAWT for private and educational and in GPLed open source
-// projects is free. For other purposes, a commercial license must be
-// obtained. There is absolutely no warranty for non-commercial use.
-//
-//
-// 1. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO
-//    WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE
-//    LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT
-//    HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM "AS IS" WITHOUT
-//    WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT
-//    NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-//    FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO THE
-//    QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE
-//    PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY
-//    SERVICING, REPAIR OR CORRECTION.
-//   
-// 2. IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN
-//    WRITING WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY
-//    MODIFY AND/OR REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE
-//    LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL,
-//    INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR
-//    INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED TO LOSS OF
-//    DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU
-//    OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY
-//    OTHER PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN
-//    ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-//   
-//    END OF TERMS AND CONDITIONS
-// 
-
 package org.me4se.impl;
 
 import javax.microedition.rms.*;
@@ -44,8 +7,13 @@ import java.io.*;
 import org.me4se.*;
 
 
-public class RecordStoreImpl_file extends RecordStoreImpl {
+public class RecordStoreImpl_file extends RecordStoreImpl implements FilenameFilter {
 
+    int version = 0;
+    long lastModified = 0L;
+
+
+    Vector listeners;
     Vector records;
     File file;
     public static File rmsDir = new File ("."); // null for applet
@@ -54,12 +22,21 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 	return rmsDir == null;
     }
 
+    public boolean accept(File dir, String name) {
+	return name.endsWith (".rms") ? true : false;
+    }
+
+    private void changeVersion () {
+	version++;
+	lastModified = System.currentTimeMillis();
+    }
+
     public void open (String recordStoreName, 
 		      boolean create) throws RecordStoreNotFoundException {
 	
 	if (refCount++ > 0) return;
 	
-	this.recordStoreName = recordStoreName;
+	this.recordStoreName = recordStoreName + ".rms";
 
 	if (isApplet ()) { 
 	    if (!create) {
@@ -70,18 +47,33 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 	    records = new Vector ();
 	}
 	else {
-	    file = new File (rmsDir, recordStoreName);
+	    file = new File (rmsDir, this.recordStoreName);
 	    
 	    try {
 
-		ObjectInputStream ois = new ObjectInputStream 
+		records = new Vector();
+
+		DataInputStream dis = new DataInputStream 
 		    (new FileInputStream (file));
 		
-		records = (Vector) ois.readObject ();
+		version = dis.readInt();
+		lastModified = dis.readLong();
 
-		//System.out.println ("read: "+records);
-		ois.close ();
+		while (true) {
+		    int length = dis.readInt();
+		    if (length > 0) {
+			byte[] buffer = new byte[length];
+			dis.readFully(buffer, 0, length);
+			records.addElement (buffer);
+		    }
+		    else {
+			records.addElement (null);
+		    }
+		}
 	    } 
+	    catch (EOFException eof) {
+		//System.out.println ("Read <" + records.size() + "> records.");
+	    }
 	    catch (Exception ioe) {
 		
 		if (!create) {
@@ -105,12 +97,24 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 	byte[] newData = new byte[numBytes];
 	System.arraycopy (data, offset, newData, 0, numBytes);
 	records.addElement (newData);
+
+	changeVersion();
+	
+	if (listeners != null) {
+	    for (int i = 0; i < listeners.size(); i++) {
+		((RecordListener)listeners.elementAt(i)).recordAdded (this, records.size());
+	    }
+	}
+	
 	return records.size ();
     }
     
 
     public void addRecordListener (RecordListener listener) {
-	throw new RuntimeException ("Not yet supported!");
+	if (listeners == null)
+	    listeners = new Vector ();
+	
+	listeners.add (listener);
     }
     
 
@@ -118,20 +122,35 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 	throws RecordStoreNotOpenException,
 	       RecordStoreException {
 
-	//System.out.println ("closeRecordStore "+recordStoreName + " / "+refCount);
-
 	if (refCount > 0) refCount--;
 
 	if (isApplet ()) return;
 
 	try {
-	    ObjectOutputStream p = new ObjectOutputStream 
-		(new FileOutputStream (file)); 
 
-	    //System.out.println ("writing: "+records);
-	    p.writeObject(records); 
-	    p.close ();
-	    records = null; 
+	    DataOutputStream dos = new DataOutputStream 
+		(new FileOutputStream (file)); 
+	    
+	    dos.writeInt (version);
+	    dos.writeLong (lastModified);
+	    
+	    for (int i = 0; i < records.size(); i++) {
+				
+		Object obj = records.elementAt(i);
+		
+		if (obj != null) {
+		    byte[] buffer = (byte[])obj;
+		    dos.writeInt (buffer.length);
+		    dos.write(buffer, 0, buffer.length);
+		}
+		else {
+		    dos.writeInt(-1);		    
+		}
+	    }
+
+	    dos.flush();
+	    dos.close();
+	    records = null;
 	}  
 	catch (IOException ioe) { 
 	    throw new RecordStoreException 
@@ -156,17 +175,20 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 
 	checkId (recordId);
 	records.setElementAt (null, recordId-1);
+	
+	changeVersion();
+
+	if (listeners != null) {
+	    for (int i = 0; i < listeners.size(); i++) {
+		((RecordListener)listeners.elementAt(i)).recordDeleted (this, recordId);
+	    }
+	}
     }
     
 
-
-    //public RecordEnumeration enumerateRecords(RecordFilter filter, RecordComparator comparator, boolean keepUpdated) {
-    //return null;
-    //}
-
-
     public long getLastModified () throws RecordStoreNotOpenException {
-	throw new RuntimeException ("Not yet supported!");
+	checkOpen();
+	return lastModified;
     }
 
 
@@ -219,23 +241,44 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 
 	return getRecord (recordId).length;
     }
-    
+
 
     public int getSize () throws RecordStoreNotOpenException {
-	throw new RuntimeException ("not yet supported");
+	checkOpen();
+	
+	int size = 0;
+	
+	// version
+	size += 4;
+	// last modified
+	size += 8;
+	
+	for (int i = 0; i < records.size(); i++) {
+	    Object obj = records.elementAt(i);
+	    // size of buffer
+	    size += 4;
+	    // the buffer
+	    if (obj != null) 
+		size += ((byte[])obj).length; 
+	    
+	}
+
+	
+	return size;
     }
     
 
     public int getSizeAvailable () throws RecordStoreNotOpenException {
-	throw new RuntimeException ("not yet supported");
+	return Integer.MAX_VALUE;
     }
     
-
+    
     public int getVersion () throws RecordStoreNotOpenException {
-	throw new RuntimeException ("not yet supported");
+	checkOpen();
+	return version;
     }
     
-
+    
     public String [] listRecordStoresImpl () {
     
 	String[] databases;
@@ -266,16 +309,21 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 	    
 
 	    databases =  directory.isDirectory()
-		? directory.list() : new String [0];
+		? directory.list(this) : new String [0];
 	}
 
+	for (int i = 0; i < databases.length; i++) {
+	    databases[i] = databases[i].substring (0, databases[i].length() - 4);
+	}
+	
 	return databases;
-}
-
-    public void removeRecordListener (RecordListener listener) {
-	throw new RuntimeException ("not yet supported");
     }
-
+    
+    public void removeRecordListener (RecordListener listener) {
+	if (listeners != null)
+	    listeners.remove (listener);
+    }
+    
 
     public void setRecord (int recordId, byte[] data, int offset, int numBytes) 
 	throws RecordStoreNotOpenException,
@@ -289,5 +337,16 @@ public class RecordStoreImpl_file extends RecordStoreImpl {
 	System.arraycopy (data, offset, newData, 0, numBytes);
 	records.setElementAt (newData, recordId-1);
 	
+	changeVersion();
+
+	if (listeners != null) {
+	    for (int i = 0; i < listeners.size(); i++) {
+		((RecordListener)listeners.elementAt(i)).recordChanged (this, recordId);
+	    }
+	}
     }
 }
+
+
+
+
