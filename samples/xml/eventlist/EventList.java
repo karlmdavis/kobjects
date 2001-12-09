a1f28d11f7c9e4ce195b6ebcd8a1d10879831bad
haustein <haustein>
2001-12-09 23:28:08 +0000

    A simple example for the minimal xml reader

---
+++ samples/xml/eventlist/EventList.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+import org.kobjects.xml.*;
+
+
+public class EventList {
+
+    public static void main (String [] args) throws IOException {
+	
+	XmlReader xr = new XmlReader 
+	    (new FileReader (args.length == 0 ? "sample.xml" : args [0]));
+	
+	while (xr.next () != XmlReader.END_DOCUMENT) {
+	    System.out.println (xr.getPositionDescription ());
+	}
+	    
+    }
+
+}
+
+
+
+
+
