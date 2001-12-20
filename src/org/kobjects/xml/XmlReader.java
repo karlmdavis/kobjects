494dfaa5f885825747095d8409139d52fee36280
haustein <haustein>
2001-12-20 10:29:49 +0000

    pushentity optimization provided by Liam Quinn

--- src/org/kobjects/xml/XmlReader.java
+++ src/org/kobjects/xml/XmlReader.java
@@ -1,8 +1,26 @@
-/* XmlReader
- *
- * The contents of this file are subject to the LGPL
- *
- * */
+// kObjects 
+//
+// Copyright (C) 2001 Stefan Haustein, Oberhausen (Rhld.), Germany
+//
+// Contributors: Liam Quinn
+//
+// License: LGPL
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+
 
 package org.kobjects.xml;
 
@@ -410,7 +428,7 @@ public class XmlReader {
 
 	for (int i = 0; i < result.length (); i++) {
 	    char c = result.charAt (i);
-	    if (result.charAt (i) > ' ') 
+	    if (c > ' ') 
 		whitespace = false;
 	    push (c);
 	}
