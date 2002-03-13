6a87548faddbd2d4363f169a2e0f0f5a7eb47db4
haustein <haustein>
2002-03-13 23:47:21 +0000

    mime decoder basis added

--- src/org/kobjects/mime/Decoder.java
+++ src/org/kobjects/mime/Decoder.java
@@ -2,13 +2,42 @@ package org.kobjects.mime;
 
 public class Decoder {
 
-    InputStream is;
+    BufferedReader reader;
+    Hashtable header;
+    byte [] binary;
+    String text;
 
-    public Decoder (InputStream is) {
+    public Decoder (Reader reader, String boundary) {
 
-	
+	this.reader = (reader instanceof BufferedReader) 
+	    ? (BufferedReader) reader
+	    : new BufferedReader (reader);
     }
 
+    
+    public boolean next () {
 
+	// read header 
 
+	header = new Hashtable ();
+
+	while (true) {
+	    String line = reader.readLine ();
+	    if (line == null || line.equals ("")) break;
+	    int cut = line.indexOf (':');
+	    if (cut == -1) 
+		throw new RuntimeException 
+		    ("colon missing in multipart header line: "+line);
+
+	    header.put (line.substring (0, cut).trim ().toLowerCase (),
+			line.substring (cut+1).trim ());
+	}
+
+	if ("base64".equals (header.get ("Content-Transfer-Encoding"))) {
+
+	}
+	else {
+	    StringBuffer buf = new StringBuffe ();
+	}
+    }
 }
