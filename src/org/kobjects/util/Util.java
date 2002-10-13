7f9f66d9c16a98cd0302b179ee292943b5060bcf
haustein <haustein>
2002-10-13 20:51:23 +0000

    *** empty log message ***

--- src/org/kobjects/util/Util.java
+++ src/org/kobjects/util/Util.java
@@ -6,21 +6,22 @@ public final class Util {
 
 	/** 
 	 * Writes the contents of the input stream to the 
-	 * given output stream and closed the input stream.
+	 * given output stream and closes the input stream.
 	 * The output stream is returned */
 
     public static OutputStream streamcopy(
         InputStream is,
         OutputStream os)
         throws IOException {
-        byte[] buf = new byte[128];
+        byte[] buf = new byte[Runtime.getRuntime ().freeMemory () >= 1048576 
+         ? 16384 : 128];
         while (true) {
-            int count = is.read(buf, 0, 128);
+            int count = is.read(buf, 0, buf.length);
             if (count == -1)
                 break;
             os.write(buf, 0, count);
         }
-        
+        is.close();        
         return os;
     }
 
